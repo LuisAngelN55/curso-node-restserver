8b6bf858ffe241ccc0a243c41fd8568df4460fef
Luis Angel Naranjo Giraldo <luis.naranjo@GRUPOMAS.COM>
2024-05-23 09:12:31 -0500

    fin sección 13

--- controllers/search.js
+++ controllers/search.js
@@ -1,6 +1,6 @@
 import { request, response } from "express";
 import { Types } from "mongoose";
-import User from "../models/user.js";
+import { User, Category, Product } from "../models/index.js";
 
 
 const availableCollectinos = [
@@ -24,6 +24,70 @@ const searchUsers = async ( term = '', res = response ) => {
         })
     }
 
+    const regex = new RegExp( term, 'i');
+
+    const users = await User.find({
+        $or: [{nombre: regex}, { correo: regex}],
+        $and: [{ estado: true }]
+    });
+
+    res.json({
+        results: users
+    })
+
+}
+
+
+const searchCategories = async ( term = '', res = response ) => {
+    
+    const isMongoID = Types.ObjectId.isValid( term );
+    console.log({isMongoID})
+
+    if ( isMongoID ) {
+        const category = await User.findById( term );
+
+        res.json({
+            results: ( category ) ? [ category ] : []
+        })
+    }
+
+    const regex = new RegExp( term, 'i');
+
+    const categories = await Category.find({
+        $or: [{name: regex}],
+        $and: [{ status: true }]
+    });
+
+    res.json({
+        results: categories
+    })
+
+}
+
+
+const searchProducts = async ( term = '', res = response ) => {
+    
+    const isMongoID = Types.ObjectId.isValid( term );
+
+    if ( isMongoID ) {
+        const product = await User.findById( term );
+
+        res.json({
+            results: ( product ) ? [ product ] : []
+        })
+    }
+
+    const regex = new RegExp( term, 'i');
+
+    const products = await Product.find({
+        $or: [{name: regex}],
+        $and: [{ status: true }]
+    });
+
+    res.json({
+        results: products
+    })
+
 }
 
 
@@ -39,15 +103,15 @@ const search = async ( req = request, res = response ) => {
 
     switch (collection) {
         case 'users':
-            searchUsers(term, res)
-        break;
-
-        case 'categories':
-
-        break;
-
-        case 'products':
+            searchUsers(term, res);
+            break;
             
+        case 'categories':
+            searchCategories(term, res);
+            break;
+            
+        case 'products':
+            searchProducts(term, res);
         break;
 
         default:
@@ -56,7 +120,7 @@ const search = async ( req = request, res = response ) => {
             })
     
     }
-
+ 
 }
 
 export {
